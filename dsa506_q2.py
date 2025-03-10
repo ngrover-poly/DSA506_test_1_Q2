b7996092282c3f179ec77f3b39becc06450575b7
ngrover-poly <ngrover@sunymaritime.edu>
2025-03-10 17:11:20 -0400

    Create dsa506_q2.py

---
+++ dsa506_q2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+import streamlit as st
+
+# KPIs
+st.title('University Students Dashboard')
+st.metric('Total Applications', students['Applications'].sum())
+st.metric('Total Admissions', students['Admitted'].sum())
+st.metric('Total Enrollment', students['Enrolled'].sum())
+
+
+# Sidebar Filters
+st.sidebar.header('Filters')
+term_filter = st.sidebar.selectbox('Select Term', ['All'] + list(students['Term'].unique()))
+filtered_students = students.copy()
+if term_filter != 'All':
+    filtered_students = filtered_students[filtered_students['Term'] == term_filter]
+school_filter = st.sidebar.selectbox('Select School',['All'] + list(filtered_students[['Arts Enrolled', 'Business Enrolled', 'Engineering Enrolled', 'Science Enrolled']].values.ravel()))
+
+def handle_input(df, school_filter): 
+    match school_filter:
+        case 'Arts':
+            return df[df['Arts Enrolled'] == school_filter]
+        case 'Business':
+            return df[df['Business Enrolled'] == school_filter]
+        case 'Engineering':
+            return df[df['Engineering Enrolled'] == school_filter]
+        case 'Science':
+            return df[df['Science Enrolled'] == school_filter]
+        case  _:
+            return df 
+            
+filtered_students = handle_input(filtered_students, school_filter)
+
+year = st.slider("Select Year:", int(students["Year"].min()), int(students["Year"].max()), int(students["Year"].min()))
